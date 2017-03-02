a26c366bc8ba4d9a0e653a0074433e91e268807e
DESKTOP-F2KMDVF\Proprietario <enrico2371@gmail.com>
2017-03-02 09:23:00 +0000

    added angular js package

--- EmployeesApplication/App_Start/RouteConfig.cs
+++ EmployeesApplication/App_Start/RouteConfig.cs
@@ -16,7 +16,8 @@ namespace EmployeesApplication
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional },
+                namespaces: new[] { "EmployeesApplication.Controllers" }
             );
         }
     }
