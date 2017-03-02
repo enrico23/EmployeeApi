19d6362a04ec69b37730e1f5680acb90da4dd6b1
DESKTOP-F2KMDVF\Proprietario <enrico2371@gmail.com>
2017-03-02 09:46:12 +0000

    added angular dataApp structure and components

---
+++ EmployeesApplication/DataApp/app.js
@@ -0,0 +1,31 @@
+﻿(function () {
+    // console.log('app inizialised');
+
+    angular.module('app', ['ngRoute'])
+        .config(config);
+
+
+    config.$inject = ['$routeProvider', '$locationProvider'];
+    function config($routeProvider, $locationProvider) {
+        $routeProvider
+            .when('/', {
+                // controller: 'HomeController',
+                templateUrl: '/dataapp/home/home.html',
+                controllerAs: 'vm'
+            })
+
+            .when('/login', {
+                // controller: 'LoginController',
+                templateUrl: '/dataapp/login/login.html',
+                // controllerAs: 'vm'
+            })
+
+            .when('/register', {
+                // controller: 'RegisterController',
+                templateUrl: '/dataapp/register/register.html',
+                // controllerAs: 'vm'
+            })
+
+            .otherwise({ redirectTo: '/login' });
+    }
+})();
